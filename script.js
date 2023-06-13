b095c66bbbfd6db59fc3c81b63cfcfd340ee1e60
Marcin <mkula.poczta@gmail.com>
2023-06-13 18:31:31 +0200

    span with Answer: added in JS

--- script.js
+++ script.js
@@ -3,33 +3,42 @@ const ballImg = document.querySelector("img")
 const answer = document.querySelector(".answer")
 const error = document.querySelector(".error")
 const allAnswers = [
-	"Who knows",
-	"Maybe yes, maybe no",
-	"Definitely",
-	"You do not want to know the answer",
-	"No",
-	"Yes",
-	"There is a good chance",
+	"Who knows!",
+	"Maybe yes, maybe no!",
+	"Definitely!",
+	"You do not want to know the answer!",
+	"No!",
+	"Yes!",
+	"There is a good chance!",
+	"I would die for that!",
 ]
 
 const checkInput = () => {
 	error.textContent = ""
+	answer.textContent = ""
 	const questionAsked = input.value
 	const lastChar = questionAsked.slice(-1)
 	if (questionAsked === "") {
 		error.textContent = "Please type your question in!"
+		answer.textContent = ""
 	} else if (lastChar !== "?") {
-		error.textContent = "Please insert ? at the end!"
+		error.textContent = "'?' is missing at the end of your question!"
+		answer.textContent = ""
 	} else {
-		answer.textContent = generateAnswer(allAnswers)
+		ballImg.classList.add("shake-anime")
+		setTimeout(function () {
+			const span = document.createElement("span")
+			span.textContent = "Answer: "
+			const reply = generateAnswer(allAnswers)
+			answer.append(span, reply)
+			ballImg.classList.remove("shake-anime")
+		}, 2000)
 	}
 }
 
 const generateAnswer = array => {
 	const index = Math.floor(Math.random() * array.length)
-	console.log(array[index])
 	return array[index]
 }
-
 
 ballImg.addEventListener("click", checkInput)
